ee79c868596c711b798d2473f8ca928e5da5932e
Shreenidhi Bhat <shreenidhi.bhat@xcdify.com>
2025-06-30 12:09:37 +0530

    fixed the hydration issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={` antialiased`}>
         <NuqsAdapter>
           <QueryWrapper>
             {children}
