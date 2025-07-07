04aaa25161cc02c0bf7d173ddb013ff058cc9310
Mujamil N <mujamil.n@xcdify.com>
2025-07-07 18:44:32 +0530

    products screen updated code

--- components/roles/index.tsx
+++ components/roles/index.tsx
@@ -150,9 +150,7 @@ export default function Roles() {
         <h1 className="text-2xl font-bold">Roles</h1>
         <Sheet open={openNew} onOpenChange={setOpenNew}>
           <SheetTrigger asChild>
-            <Button onClick={() => setOpenNew(true)}>
-              <Plus className="mr-2 h-4 w-4" />Add Role
-            </Button>
+           
           </SheetTrigger>
           <SheetContent side="right" className="w-full sm:w-full md:!max-w-[50%] lg:!max-w-[22%] overflow-hidden">
             <SheetHeader>
